cf1841e1f99132291ba8378ce1efa3f70e4908d5
Guilherme Samuel <guisamuel800@gmail.com>
2023-01-05 19:45:19 -0300

    __init__ with class (not working)

--- wpimath/util/__init__.py
+++ wpimath/util/__init__.py
@@ -1,1 +1,18 @@
-
+from .units.Units import (
+    degreesToRadians,
+    degreesToRotations,
+    feetToMeters,
+    inchesToMeters,
+    kilogramsToLbs,
+    lbsToKilograms,
+    metersToFeet,
+    metersToInches,
+    millisecondsToSeconds,
+    radiansPerSecondToRotationsPerMinute,
+    radiansToDegrees,
+   	radiansToRotation,
+   	rotationsPerMinuteToRadiansPerSecond,
+    rotationsToDegrees,
+    rotationsToRadians,
+   	secondsToMilliseconds
+)
